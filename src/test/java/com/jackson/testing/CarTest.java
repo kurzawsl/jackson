13835536f161b679c34b24e406f7a17dc6573050
Lukasz Kurzawski <lkurzawski@gmail.com>
2020-11-07 12:29:30 +0100

    Object to json and json to object

---
+++ src/test/java/com/jackson/testing/CarTest.java
@@ -0,0 +1,34 @@
+package com.jackson.testing;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class CarTest {
+    ObjectMapper objectMapper = new ObjectMapper();
+
+
+    @Test
+    @DisplayName("Class To JSON")
+    void classToJSON() throws IOException {
+        objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+        Car car = new Car("yellow", "renault");
+        objectMapper.writeValue(new File("car.json"), car);
+    }
+
+    @Test
+    void jsonToObject() throws IOException {
+        String json = "{ \"color\" : \"Black\", \"type\" : \"BMW\" }";
+        Car car = objectMapper.readValue(json, Car.class);
+        assertThat(car.getColor()).isEqualTo("Black");
+    }
+
+}
